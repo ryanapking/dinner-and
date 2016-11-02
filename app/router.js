c031fd3f40aa9df63636b53347793ac0b16cc55b
Alaina Traxler <eloavox@gmail.com>
2016-11-02 12:30:42 -0700

    add user dynamic route.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('user');
+  this.route('user', {path: '/user/:user_id'});
   this.route('messages');
   this.route('events');
   this.route('eventlist');
