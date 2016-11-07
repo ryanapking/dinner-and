947b2a15712a6a52a45b5a3c5014a6cc9185b094
Alaina Traxler and Justin Kincaid <alaina@>
2016-11-07 15:56:12 -0800

    add user auth for invites to events

--- app/helpers/is-logged-in.js
+++ app/helpers/is-logged-in.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export function isLoggedIn(params) {
+  console.log("Host ID: " + params[0]);
+  console.log("Session ID: " + params[1]);
   if(params[0] === params[1]){
     return true;
   }else {
