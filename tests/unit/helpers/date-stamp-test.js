98df0d96b4ef72fc9805723df54f1546dabd860b
HK Kahng and Ryan Apking <ryan@>
2016-11-08 15:48:12 -0800

    add helper that parses string date and include this on the maps page.

---
+++ tests/unit/helpers/date-stamp-test.js
@@ -0,0 +1,12 @@
+
+import { dateStamp } from 'dinner-and/helpers/date-stamp';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | date stamp');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = dateStamp([42]);
+  assert.ok(result);
+});
+
