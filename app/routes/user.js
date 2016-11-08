7e287370ea2d5190e49f701ddca6ff85224c6c6d
Alaina Traxler and Justin Kincaid <alaina+justin@>
2016-11-08 10:03:28 -0800

    add spalsh page and application controller.

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentPath: "",
+  afterModel(){
+    this.set("currentPath", "test");
+    // console.log(this.get("routeName"));
+  },
   addInterests:[],
   model(params){
     return Ember.RSVP.hash({
@@ -40,7 +45,7 @@ export default Ember.Route.extend({
         })
       })
     },
-    togglebutton(_interest, _interestID, _userID){
+    togglebutton(_interest, _interestID){
       if(!($("#" + _interestID).hasClass("basic"))){
         $("#" + _interestID).addClass("basic");
         this.addInterests.splice(this.addInterests.indexOf(_interest),1);
@@ -49,6 +54,10 @@ export default Ember.Route.extend({
         console.log(this.addInterests);
         $("#" + _interestID).removeClass("basic");
       }
+    },
+    testFunc(){
+      this.set("currentPath", "test");
+      console.log(this.get(this.currentPath));
     }
   }
 });
